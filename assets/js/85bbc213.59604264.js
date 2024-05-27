348465c36659d4f910c5c45ac9be4603473d1ea0
SkylarShi <yus029@ucsd.edu>
2024-05-26 20:41:15 -0700

    Deploy website - based on 719ab5f9646a64ea9879b01931410f8adbfe648a

---
+++ assets/js/85bbc213.59604264.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbpd_reference=self.webpackChunkbpd_reference||[]).push([[9797],{2e3:e=>{e.exports=JSON.parse('{"permalink":"/bpd-reference/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
---
+++ assets/js/cd451366.27038027.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbpd_reference=self.webpackChunkbpd_reference||[]).push([[5391],{8539:e=>{e.exports=JSON.parse('{"permalink":"/bpd-reference/blog/tags/docusaurus","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
